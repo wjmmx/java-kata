32da289c65403ae1e8228fe749707ad5bd31c1a2
philip.wang <philip.wang@perficientgdc.com.cn>
2017-09-11 03:10:17 +0800

    AreaCalculator test failed

---
+++ test/agiledeveloper/kata/lspViolations/AreaCalculatorTest.java
@@ -0,0 +1,25 @@
+package agiledeveloper.kata.lspViolations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreaCalculatorTest {
+
+    @Test
+    public void areaIs36forRectangle4asLength9asBreadth() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setBreadth(9);
+        assertEquals(36, rectangle.getArea());
+    }
+
+    @Test
+    public void areaIs36forSquare4asLength9asBreadth() {
+        Rectangle rectangle = new Square();
+        rectangle.setLength(4);
+        rectangle.setBreadth(9);
+        assertEquals(36, rectangle.getArea());
+    }
+
+}
